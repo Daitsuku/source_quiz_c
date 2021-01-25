33431136a8bd49c7b039bfb75c6172a49067a50d
Daitsuku <donlawat0@gmail.com>
2021-01-25 14:42:31 +0700

    first commit

---
+++ reverse_num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+    system("cls");
+
+    int n,i;
+    printf("Please insert num : ");
+    scanf("%d",&n);
+
+//core
+    i = 0;
+    while (i != n){
+    printf("%d ",n);
+    n--;
+    }   
+}
